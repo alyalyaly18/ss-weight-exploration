56cd50c7ee0ab28529da45b0e8fb57b177e36423
Alyssa Reyes <reyesalyssa18@gmail.com>
2023-02-01 10:09:55 -0800

    created star background and centered planet with browser window

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -13,13 +13,28 @@ const Planet = () => {
     p5.setup = () => {
       p5.createCanvas(p5.windowWidth, p5.windowHeight);
       p5.background(0);
+      p5.randomSeed(700);
+      for (var j = 0; j <= 700; j++) {
+        p5.fill(
+          p5.random(120, 255),
+          p5.random(120, 255),
+          p5.random(120, 255),
+          p5.random(80, 150)
+        );
+        p5.circle(
+          p5.random(p5.windowWidth),
+          p5.random(p5.windowHeight),
+          p5.random(4)
+        );
+      }
     };
 
     p5.draw = () => {
       // if (color === "mars") {
       //   p5.fill(255,0,0)
       // }; // Conditional based on passed
-      p5.circle(400, 400, 100);
+      p5.ellipseMode(p5.CENTER);
+      p5.ellipse(p5.windowWidth / 2, p5.windowHeight / 2, 300);
     };
   };
   useEffect(() => {
